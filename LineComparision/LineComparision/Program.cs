8b7fa2cfb9a8c2f1d839540fad22b30b34905ea1
Harinilekkala <harini.lekkala1997@gmail.com>
2022-05-17 16:11:05 +0530

    [Harini]: check length of the line

---
+++ LineComparision/LineComparision/Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+LineComparision.lineclass linecomparison = new LineComparision.lineclass();
+double length = linecomparison.getLength(2, 3, 2, 8);
+Console.WriteLine("length of the given line is" + length);
+
