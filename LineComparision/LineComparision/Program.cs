dfa0c2d2985862573d45aee3c4fa77fdf732e180
Harinilekkala <harini.lekkala1997@gmail.com>
2022-05-17 16:31:42 +0530

    [Harini]: check equality of two lines

--- LineComparision/LineComparision/Program.cs
+++ LineComparision/LineComparision/Program.cs
@@ -1,6 +1,8 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
 LineComparision.lineclass linecomparison = new LineComparision.lineclass();
-double length = linecomparison.getLength(2, 3, 2, 8);
-Console.WriteLine("length of the given line is" + length);
+double length1 = linecomparison.getLength(2, 3, 2, 8);
+double length2 = linecomparison.getLength(2, 5, 2, 6);  
+Console.WriteLine("length of the given line is" + length1,length2);
+Console.WriteLine(length1.Equals(length2));
 
