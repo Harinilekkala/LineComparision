74a0351bb5b331e1e29d789991523b1ffb2d7ea4
Harinilekkala <harini.lekkala1997@gmail.com>
2022-05-17 16:44:15 +0530

    [Harini]: Check comparision of lines

--- LineComparision/LineComparision/Program.cs
+++ LineComparision/LineComparision/Program.cs
@@ -5,4 +5,10 @@ double length1 = linecomparison.getLength(2, 3, 2, 8);
 double length2 = linecomparison.getLength(2, 5, 2, 6);  
 Console.WriteLine("length of the given line is" + length1,length2);
 Console.WriteLine(length1.Equals(length2));
-
+Console.WriteLine(length1.CompareTo(length2));
+if (length1.CompareTo(length2) > 0)
+    Console.WriteLine("The line one is greater than line two");
+else if (length1.CompareTo(length2) < 0)
+    Console.WriteLine("The line 2 is greater than line 1");
+else
+    Console.WriteLine("Both lines are same");
